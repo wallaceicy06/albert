1ee861a89f5b6382d22d593f422d33e86f450652
Sean Harger <seanharger93@gmail.com>
2015-07-28 09:18:13 -0700

    Added moment for time and some minor changes.

--- ember/app/controllers/user.js
+++ ember/app/controllers/user.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  fullName: function() {
+    var user = this.get('model');
+
+    return user.get('firstName') + ' ' + user.get('lastName');
+  }.property('model.firstName', 'model.lastName')
 });
