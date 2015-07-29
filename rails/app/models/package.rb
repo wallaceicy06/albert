a0d1489df4ad8d4da3ea42cf8c3b99f0a527ab09
Sean Harger <seanharger93@gmail.com>
2015-07-28 22:22:28 -0700

    Fixed bug disallowing duplicate barcodes when picking up package.

--- rails/app/models/package.rb
+++ rails/app/models/package.rb
@@ -2,7 +2,7 @@ class Package < ActiveRecord::Base
   belongs_to :owner, class_name: 'User', foreign_key: :owner_id
   belongs_to :processor, class_name: 'User', foreign_key: :processor_id
 
-  validates :barcode, presence: true, uniqueness: true
+  validates :barcode, on: :create, presence: true, uniqueness: { scope: :picked_up }
   validates :processor, presence: true
   validates :owner, presence: true
   validate :processor_is_admin
